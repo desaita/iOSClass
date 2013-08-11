3819aa33117bf100d98981a8cfffe683ed18f7f8
Tanvi Desai <tanvidesai@Tanvis-MacBook-Air.local>
2013-08-11 09:18:19 -0400

    Initial Commit

---
+++ ObjectRecipes/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  ObjectRecipes
+//
+//  Created by Tanvi Desai on 8/11/13.
+//  Copyright (c) 2013 Tanvi Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectRecipes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectRecipes
+//
+//  Created by Tanvi Desai on 8/11/13.
+//  Copyright (c) 2013 Tanvi Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}
